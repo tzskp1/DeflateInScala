3b7b5b7424fa8a45fdcde7ab8eb93022ffa096e5
Tanaka Kazunari <tzskp1@gmail.com>
2018-04-22 22:22:01 +0900

    enable to work.

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -2,29 +2,78 @@ package example
 
 object Huffman {
   type Sym = Int
-  case class Chain(weight: Int,totalCnt: Int,symCnt: Int,next: List[(Sym,Int)],child: Option[Chain])
+  case class Chain(weight: Int,symCnt: Int,child: Option[Chain])
+  case class Stage(left: Chain,right: Chain,rest: List[(Sym,Int)])
 
   val sizeOfLitLen = 286
-  val maxCodeLen = 15
-  def histToLengths(hist: List[(Sym,Int)]) = {
-    //val stage0 = hist.sortBy{case (x,c) => c}
-    val num = hist.length
-    def calcNextChain(stages: List[Chain]) : List[Chain] = {
+  val maxCodeLen = 3 //15
+
+  def updateStage(stages: List[Stage],cnt: Int) : List[Stage] = {
+    if (cnt >= 2) {
+      stages
+    } else {
       stages match {
         case Nil => Nil
-        case Chain(_,totalCnt,symCnt,(_,lookahead1: Int)::((_,lookahead2: Int)::(base2: List[(Sym,Int)]) as base1) as orig,child) ::
-          ((Chain(weight,_,_,_,_) as pre) :: _ as (rest : List[Chain])) =>
-          if (lookahead1 < weight) {
-            if (lookahead2 < weight) {
-              calcNextChain(Chain(lookahead2, totalCnt + 2, symCnt + 2, base2, child) :: rest)
-            } else {
-              calcNextChain(Chain(lookahead1, totalCnt + 1, symCnt + 1, base1, child) :: rest)
-            }
-          } else {
-            Chain(weight,totalCnt+1,symCnt,orig,Some(pre)) :: calcNextChain(rest)
+        case Stage(_, Chain(weight, symCnt, child), Nil) :: Nil => stages // ??
+        case Stage(_, Chain(weight, symCnt, child), (_, lookahead) :: next) :: Nil =>
+          updateStage(Stage(Chain(weight, symCnt, child), Chain(lookahead, symCnt + 1, child), next) :: Nil, cnt + 1)
+        case Stage(_, Chain(weight, symCnt, child), (sym, lookahead) :: next) :: pre :: rest =>
+          pre match {
+            case Stage(Chain(weightLeft, _, _), Chain(weightRight, preSymCnt, preChild), _) =>
+              if (lookahead < weightRight + weightLeft) {
+                updateStage(Stage(Chain(weight, symCnt, child), Chain(lookahead, symCnt + 1, child), next) :: pre :: rest, cnt + 1)
+              } else {
+                updateStage(Stage(Chain(weight, symCnt, child),
+                  Chain(weightLeft + weightRight, symCnt, Some(Chain(weightRight, preSymCnt, preChild))) , (sym, lookahead) :: next)
+                  :: updateStage(pre :: rest, 0), cnt + 1)
+                // TODO: refactor
+              }
+          }
+        case Stage(_, Chain(weight, symCnt, child), Nil) :: pre :: rest =>
+          pre match {
+            case Stage(Chain(weightLeft, _, _), Chain(weightRight, preSymCnt, preChild), _) =>
+              updateStage(Stage(Chain(weight, symCnt, child),
+                Chain(weightLeft + weightRight, symCnt, Some(Chain(weightRight, preSymCnt, preChild))), Nil) :: updateStage(pre :: rest, 0), cnt + 1)
+            // TODO: refactor
           }
       }
     }
+  }
+  def initStage(hist: List[(Sym,Int)]) = {
+    hist match {
+      case (_,lookahead1) :: (_,lookahead2) :: rest =>
+        Stage(Chain(lookahead1,1,None),Chain(lookahead2,2,None),rest)
+    }
+  }
+  def traverseChain(ch: Chain) : List[Int] = {
+    ch match {
+      case Chain(_,symCnt,None) =>
+        symCnt :: Nil
+      case Chain(_,symCnt,Some(child)) =>
+        symCnt :: traverseChain(child)
+    }
+  }
+  def histToLengths(hist: List[(Sym,Int)]) = {
+    //val stage0 = hist.sortBy{case (x,c) => c}
+    val num = hist.length
+    if (num < 2)
+      throw new Exception
+      // TODO: error handling
+    val stages = for (_ <- (1 to maxCodeLen).toList) yield { initStage(hist) }
+    val lastChain = (1 to num - 2).foldLeft(stages){ (acc,_) => updateStage(acc,0) }.head match {
+      case Stage(_,right,_) => right
+    }
+    var res = new Array[Int](sizeOfLitLen)
+    for (i <- traverseChain(lastChain)) {
+      hist.foldLeft(0){
+        case (cnt,(sym,_)) =>
+          if (cnt < i) {
+            res(sym) += 1
+          }
+          cnt + 1
+      }
+    }
+    res
   }
   // lengths of code of char -> codes of char
   def makeCanonHuffmanTable(src: Array[Int]) = {
@@ -78,10 +127,19 @@ object Huffman {
           println(ind.toChar + " " + cnt.toString)
     }
   }
+  def test2 () = {
+    val hist = List(('A':Sym,2),('E':Sym,4),('B':Sym,6),('D':Sym,8),('C':Sym,10))
+    val buf = histToLengths(hist)
+    buf.zipWithIndex.foreach {
+      case (cnt,ind) =>
+        if (cnt != 0)
+          println(ind.toChar + " " + cnt.toString)
+    }
+  }
 }
 
 object Hello extends Greeting with App {
-  Huffman.test()
+  Huffman.test2()
 }
 
 trait Greeting {
